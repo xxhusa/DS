e42da0ad770f7621f2f75570cdaeb1f3718dd5cb
unknown <lab1069@etecjk.local>
2025-09-30 22:27:53 -0300

    atividade fila

---
+++ Painel/Painel/Paciente.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Painel
+{
+    public class Paciente : Pessoa
+    {
+        public int idade;
+
+
+        public static void Cadastrar()
+        {
+            Console.WriteLine("Qual seu nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Qual sua idade:");
+            int idade = int.Parse(Console.ReadLine());
+
+
+            if (idade >= 60)
+            {
+
+                Console.WriteLine("Prioridade 1");
+
+            }
+            else if (idade <= 17)
+            {
+
+                Console.WriteLine("Prioridade 2");
+
+            }
+            else
+            {
+
+                Console.WriteLine("Prioridade 3");
+            }
+            Console.WriteLine($"Cadastro realizado com sucesso! nome = {nome} ,idade =  {idade},");
+        }
+    }
+}
---
+++ Painel/Painel/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Painel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int opcao;
+            
+
+            do
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("1 . Cadastrar Paciente");
+                Console.WriteLine("2 . Listar pacientes");
+                Console.WriteLine("3 . Atender pacientes da fila");
+                Console.WriteLine("4 . Alterar Dados Cadastrais");
+                Console.WriteLine("0 . Sair");
+                Console.WriteLine("Digite sua opção:");
+
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+
+                    case 1:
+                        Paciente p1  = new Paciente();
+                        p1.Cadastrar();
+                        break;
+                    case 2:
+                        Console.WriteLine("tudo bem");
+                        break;
+                    case 3:
+                        Console.WriteLine("como vai");
+                        break;
+                    case 4:
+                        Console.WriteLine("olaa");
+                        break;
+                    case 0:
+                        Console.WriteLine("saindo do programa");
+                        break;
+                    default:
+                        Console.WriteLine("opcao invalida");
+                        break;
+                } 
+            } while (opcao != 0);
+
+            Pessoa [] paciente2 = new Pessoa[15];
+
+
+        }
+    }
+}
+
+
+
+
+
+
